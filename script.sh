38aa4abd599d712ed46971caf6defd8e846ab3ab
RabiaNoumanSiddiq <99790208+RabiaNoumanSiddiq@users.noreply.github.com>
2024-03-15 13:33:18 +0500

    changes

--- script.sh
+++ script.sh
@@ -14,20 +14,4 @@ sudo usermod -aG sudo "$username"
 echo "$username ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/$username"
 
 # Switch to the new user and execute additional commands
-sudo -u "$username" bash << EOF
-echo "User: \$USER"
-# Add your additional commands here
-sudo apt update
-clear
-sudo apt install -y software-properties-common
-clear
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-clear
-sudo apt update
-clear
-sudo apt -y install code
-clear
-export PATH="/usr/shared/code/bin:$PATH"
-echo $PATH
-clear
-EOF
+sudo -u "$username"
